4a68f5a9059488320d551dff755a8b52fef46abc
HidayaSyam <hid.sy1910@gmail.com>
2020-06-13 19:16:54 +0300

    q3 Hidaya

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -32,11 +32,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var mul=a*b;   
+   var result=[mul,'The product of '+a+ ' and '+b+' is ' +mul +'.'];    
+  console.log(result);   
+  
+  return result;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -53,11 +57,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sum=a+b+c;    
+  var mul=a*b*c;    
+  var result=[sum,mul,a+' and '+b+' and '+c+' sum to '+sum+'.','The product of '+a+ ' and '+b+' and '+c+' is ' +mul +'.'];    
+  console.log(result);    
+  return result;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
